bd5296c35f3279b604701c4566500f2d5714c7f6
DE <DE@10.20.50.83>
2014-01-14 17:30:53 -0600

    Button pressed and released  done

--- src/edu/wpi/first/wpilibj/templates/DriveStick.java
+++ src/edu/wpi/first/wpilibj/templates/DriveStick.java
@@ -17,13 +17,36 @@ import java.lang.Math;
 public class DriveStick extends Joystick{
     
     double deadZone,xAxis,yAxis,twist;
+    boolean buttonPressed[],buttonReleased[];
     
     public DriveStick(int port,double deadZoneIn){
         super(port);
         
         deadZone=deadZoneIn;
+        
+        buttonReleased= new boolean[13];
+        
+        for(int x=0;x<=12;x++){
+           buttonReleased[x]= false;
+        }
+        
+        buttonPressed = new boolean[13];
+        
+        for(int x=0;x<=12;x++){
+           buttonPressed[x]= false;
+        }
+        
     }
     public void update(){
+        
+        for(int x=1;x<=12;x++){
+           buttonReleased[x]= buttonPressed[x]&&!getRawButton(x);
+        }
+        
+        for(int x=1;x<=12;x++){
+           buttonPressed[x]= getRawButton(x);
+        }
+        
         xAxis=getAxis(Joystick.AxisType.kX);
         yAxis=getAxis(Joystick.AxisType.kY);
         twist=getAxis(Joystick.AxisType.kTwist);
@@ -40,4 +63,11 @@ public class DriveStick extends Joystick{
         return Math.abs(twist)>deadZone ? twist:0;
     }
 
+    public boolean getButtonPressed(int in){
+        return buttonPressed[in];
+    }
+    
+    public boolean getButtonReleased(int in){
+        return buttonReleased[in];
+    }
 }
